a23d266d9dc2ca8aa2691c1b0042ee82702428bd
thakur-shivam <shivamrudra2006@gmail.com>
2026-02-09 00:50:19 +0530

    add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import Courses from "./_components/Courses";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h2>AI Video Course Generator</h2>
-      <Button>All Courses</Button>
-      <UserButton />
+      <Header />
+      <Hero />
+      <Courses />
+      <div className="absolute -bottom-40 -left-40 h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full" />
+      <div className="absolute top-20 left-1/3 bottom-[-200px] h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full" />
+      <div className="absolute bottom-[-200px] left-1/3 h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full" />
+      <div className="absolute top-[200px] left-1/2 h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full" />
     </div>
   );
 }
