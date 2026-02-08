284c7d967c33b8347f3ac0f4430014684ae98187
thakur-shivam <shivamrudra2006@gmail.com>
2026-02-08 18:49:43 +0530

    configure authentication using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -27,12 +29,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={AppFront.className}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={AppFront.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ app/provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { UserContext } from '@/contexts/user';
+
+function Provider({ children }: { children: React.ReactNode }) {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        createNewUser();
+    }, []);
+
+    const createNewUser = async () => {
+        // user API endpoint call to create a new user
+        const response = await axios.post('/api/user', {});
+        setUser(response.data);
+    }
+  return (
+    <div>
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default Provider;
